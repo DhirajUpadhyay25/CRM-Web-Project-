509c157dc60a01975018711d0d1f71024437eacb
Dhiraj Upadhyay <dhirajofficial2060@gmail.com>
2026-08-30 00:02:40 +0530

    feat: implement automatic data seeding on application startup

--- src/main/java/in/project/main/EducationApplication.java
+++ src/main/java/in/project/main/EducationApplication.java
@@ -1,13 +1,29 @@
 package in.project.main;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import in.project.main.services.DataSeederService;
 
 @SpringBootApplication
 public class EducationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EducationApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner seedData(DataSeederService dataSeederService) {
+		return args -> {
+			try {
+				dataSeederService.seedAll();
+				System.out.println("=== Data seeding completed ===");
+			} catch (Exception e) {
+				System.err.println("Data seeding failed: " + e.getMessage());
+			}
+		};
 	}
 
 }
